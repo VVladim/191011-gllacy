b1d491dac0b47e40da8694d8ceeb1fc395b48420
VVladim <vladim.volovenko@yandex.ru>
2016-09-05 19:12:28 +0300

    почистил код

--- js/popup.js
+++ js/popup.js
@@ -5,18 +5,12 @@ var close = popup.querySelector(".modal-content-close");
 var login = popup.querySelector("[name=your-name]");
 var form = popup.querySelector("form");
 var mail = popup.querySelector("[name=mail]");
-var storage = localStorage.getItem("login");
+// var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal-content-show");
-  if (storage) {
-    login.value = storage;
-    mail.focus();
-  } else {
-    login.focus();
-  }
- });
+});
 
  close.addEventListener("click", function(event) {
    event.preventDefault();
@@ -26,10 +20,10 @@ link.addEventListener("click", function(event) {
 
 form.addEventListener("submit", function(event) {
   if (!login.value || !mail.value) {
-  event.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
+    event.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     localStorage.setItem("login", login.value)
   }
